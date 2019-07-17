6405ed6610d6a7598c184aee411cb5779a3b013f
DhengreAni18 <dhengreani18@gmail.com>
2019-07-17 12:42:11 +0530

    Starting of App

---
+++ src/components/header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {
+    SafeAreaView,
+    StyleSheet,
+    ScrollView,
+    View,
+    Text,
+    StatusBar,
+  } from 'react-native';
+
+  const Header = () => {
+      return(
+          <Text>Albums!</Text>
+      );
+  }

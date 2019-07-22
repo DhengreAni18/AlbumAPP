ff9af35ca66b9094db7a68a38fa4fed0ab19d1dd
DhengreAni18 <dhengreani18@gmail.com>
2019-07-22 23:59:36 +0530

    Data response

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,15 +1,21 @@
-import React, {Component} from 'react';
-import { View, Text } from 'react-native';
+import React, { Component } from "react";
+import { View, Text } from "react-native";
+import axios from 'axios';
 
 class AlbumList extends Component {
-    render() {
-        return (
-            <View>
-                <Text>Album List ! </Text>
-            </View>
-        );
-    }
-};
+  componentWillMount() {   //Run as the app starts
+    axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+        .then(response => console.log(response));  
+  
+  } 
 
+  render() {
+    return (
+      <View>
+        <Text>Album List!!!  </Text>
+      </View>
+    );
+  }
+}
 
 export default AlbumList;

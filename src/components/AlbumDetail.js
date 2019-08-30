5870c322b37a83467b547d4315cb31ee1258e91e
DhengreAni18 <dhengreani18@gmail.com>
2019-08-30 16:26:05 +0530

    Added styles

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -5,23 +5,31 @@ import CardSection from './CardSection';
 
 const AlbumDetail = ({ album }) => {
 
-    const {title , artist , thumbnail_image } = album;
+    const {title , artist , thumbnail_image,image } = album;
+    // const {thumbnailContainer , thumbnailContainer , headerStyle ,textStyleHead , imageStyle}
 
    return (
        <Card>
            <CardSection>
-               <View>
+
+               <View style={styles.thumbnailContainer}>
                    <Image style = {styles.imageStyle} source = {{ uri: thumbnail_image }} />
                </View>
                <View style = { styles.headerStyle} >
-               <Text>{title}</Text>
+               <Text style={styles.textStyleHead}>{title}</Text>
                <Text>{artist}</Text>     
                </View>
            
            </CardSection>
+
+           <CardSection> 
+               <Image style={styles.imageMainStyle} source = {{uri:image}} />
+           </CardSection>
        </Card>
    );
 };
+
+
 
 
 const styles = {
@@ -29,10 +37,23 @@ const styles = {
         flexDirection: 'column',
         justifyContent: 'space-around'
     },
-
+    textStyleHead: {
+        fontSize: 20,
+    },
     imageStyle: {
         height:50,
         width:50
+    },
+    thumbnailContainer: {
+        justifyContent: 'center',
+        alignItems : 'center',
+        marginLeft: 10,
+        marginRight: 10,
+    },
+    imageMainStyle : {
+        height:300,
+        flex:1,
+        width:null
     }
 };
 
